156bca77364b801fbe2138e2741137b443746bcf
Conor Walsh <cw.conorwalsh@gmail.com>
2015-11-04 11:53:38 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,20 @@ class UsersController < ApplicationController
       def show
         @user = User.find(params[:id])
       end
-
       def new
+        @user = User.new
       end
-    end
+	 def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+           # Handle a successful save.
+		    flash[:success] = "Welcome to the Conor's Twitter App!"
+		    redirect_to @user
+        else
+            # Handle an unsuccessful save.
+            render 'new'			
+        end
+      end
+ end
